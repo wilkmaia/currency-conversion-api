baeeb9cdfe753c25dbd629b70d368cdfa7a3e119
Wilk Maia <wilkmaia@gmail.com>
2016-04-29 10:18:37 -0300

    Version 1.0.0

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require( "express" );
+var request = require( "request" );
+
+var app = express();
+var base = "BRL";
+var options = [ "USD", "CAD", "GBP", "EUR", "ARS", "MYR", "JPY" ];
+
+var conv = [];
+
+app.get( '/getEverything', function( req, res ){
+	o = options[0];
+	
+	options.forEach( function( o ){
+		request( "https://www.google.com/finance/converter?a=1&from="+ o +"&to=" + base,
+		function( er, response, body ){ // response function
+			//res.push( parseFloat( res.body.split( "<span class=bld>" )[1] ) );
+			responseGetEverything( parseFloat( response.body.split( "<span class=bld>" )[1] ), res );
+		});
+	});
+});
+
+function responseGetEverything( val, res )
+{
+	conv.push( val );
+	if( conv.length == options.length ) // All requests done
+	{
+		var data = {};
+		options.forEach( function( o, idx, ar ){
+			data[o] = conv[idx];
+			if( idx == ar.length - 1 )
+				res.end( JSON.stringify( data ) );
+		});
+	}
+}
+
+
+app.get( '/getCustomConversion', function( req, res ){
+	var f = req.query.from;
+	var t = req.query.to;
+	
+	request( "https://www.google.com/finance/converter?a=1&from="+ f +"&to=" + t,
+	function( er, response, body ){ // response function
+		res.end( parseFloat( response.body.split( "<span class=bld>" )[1] ).toString() );
+	});
+});
+
+var server = app.listen( 9980, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	
+	console.log( "Currency converter app listening at http://%s:%s", host, port );
+});
